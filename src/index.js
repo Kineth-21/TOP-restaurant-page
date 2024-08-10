bda4fe96e745f1f3a4da29a50fa047779b2c3d78
Kenneth <kenneth21tay@gmail.com>
2024-08-10 13:49:25 +0800

    Edit images in images folder

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './style.css';
+// import test from "./images/chicken_feet.jpg"
 
-console.log("You've been served!");
+// console.log("You've been served!");
+// const content = document.querySelector("#content");
+
+// const myIcon = new Image();
+// myIcon.src = test;
+
+// content.appendChild(myIcon);
+
