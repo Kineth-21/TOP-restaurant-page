3d28ba1b51a562dad5e69503920aba060f7c260d
Kenneth <kenneth21tay@gmail.com>
2024-08-10 22:28:37 +0800

    Added file generator for images

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,12 @@ module.exports = {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: 'asset/resource',
             generator: {
-                filename: 'images/[name][hash][ext]', // Output directory and filename pattern for images
+                filename: 'images/[name][hash][ext]',
             },
         },
         {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
         },
         ],
     },
